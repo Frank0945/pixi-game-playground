b38383a4dc8803955bbcd1e76d0cf4722edb4970
Frank <40611willy@gmail.com>
2023-12-30 10:26:02 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { useEffect, useState } from "react";
-import { _ReactPixi, Container, Sprite } from "@pixi/react";
 
-import bunnySprite from "./assets/bunny.png";
 
 const App = () => {
-  const [bunny, setBunny] = useState<_ReactPixi.ISprite>();
-
-  useEffect(() => {
-    setBunny({
-      x: 100,
-      rotation: 5,
-    });
-  }, []);
-
+ 
   return (
-    <Container>
-      <Sprite image={bunnySprite} y={82} {...bunny} />
-    </Container>
+    <></>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,13 @@
 import { StrictMode } from "react";
-import { Stage } from "@pixi/react";
-import { Color } from "pixi.js";
 import { createRoot } from "react-dom/client";
 
 import "./style.css";
+import "./initial";
 
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Stage
-      options={{
-        backgroundColor: new Color({ r: 0, g: 0, b: 0 }).toArray(),
-        resizeTo: window,
-      }}
-      width={window.innerWidth}
-      height={window.innerHeight}
-    >
-      <App />
-    </Stage>
+    <App />
   </StrictMode>,
 );
